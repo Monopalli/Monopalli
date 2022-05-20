ceb36fd032312fe9a68646dc24318339f2563f20
Pattypan <gulati.parth98@gmail.com>
2022-05-21 00:38:34 +0530

    Created the superior, responsive navbar which doesn't have shadows because some bitch decided to make the theme that way

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,83 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+import MenuItem from '@mui/material/MenuItem';
+import CustomButton from '../Button/Button'
+
+const actions = ['Login', 'Sign Up'];
+
+const Header = () => {
+    const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
+
+    const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorElNav(event.currentTarget);
+    };
+
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
+
+    return (
+        <AppBar elevation={4} color="transparent" position="static">
+            <Container maxWidth="xl">
+                <Toolbar disableGutters>
+                    <CustomButton variant="contained">
+                        Monapalli for Podapattis
+                    </CustomButton>
+                    <Box sx={{ flexGrow: 1 }}>
+
+                    </Box>
+                    <Box sx={{ flexGrow: 0, display: { xs: 'flex', md: 'none' } }}>
+                        <IconButton
+                            size="large"
+                            aria-label="account of current user"
+                            aria-controls="menu-appbar"
+                            aria-haspopup="true"
+                            onClick={handleOpenNavMenu}
+                            color="inherit"
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Menu
+                            id="menu-appbar"
+                            anchorEl={anchorElNav}
+                            anchorOrigin={{
+                                vertical: 'bottom',
+                                horizontal: 'left',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'left',
+                            }}
+                            open={Boolean(anchorElNav)}
+                            onClose={handleCloseNavMenu}
+                            sx={{
+                                display: { xs: 'block', md: 'none' },
+                            }}
+                        >
+                            {actions.map((page) => (
+                                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                                    <Typography textAlign="center">{page}</Typography>
+                                </MenuItem>
+                            ))}
+                        </Menu>
+                    </Box>
+
+                    <Box sx={{display: { xs: 'none', md: 'flex' }, flexGrow: 0, justifyContent: 'space-between'}}>
+                        <Button sx={{marginRight: '10px'}} variant="outlined">Login</Button>
+                        <Button variant="contained">Sign Up</Button>
+                    </Box>
+                </Toolbar>
+            </Container>
+        </AppBar>
+    );
+};
+export default Header;
